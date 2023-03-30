b90a61105e29b6904ea65ca90bfbc124585cb85e
Madux <kingstongerald@gmail.com>
2023-03-30 22:55:27 +0100

    [IMP] Added button method to employee

--- hr_pms/models/hr_employee.py
+++ hr_pms/models/hr_employee.py
@@ -59,32 +59,33 @@ class HrEmployee(models.Model):
     #         rec.number_commendation = sum([amt.number_commendation for amt in rec.appraisal_ids])
     #         rec.number_absent = sum([amt.number_absent for amt in rec.appraisal_ids])
 
-    # def open_employee_appraisals(self):
-    #     for rec in self:
-    #         appraisals = self.env['usl.employee.appraisal'].search([('employee_id', '=', self.id)])
-    #         emp_appraisal = [rec.id for rec in appraisals] if appraisals else []
-    #         form_view_ref = self.env.ref('maach_hr_appraisal.usl_employee_appraisal_form_view', False)
-    #         tree_view_ref = self.env.ref('maach_hr_appraisal.view_usl_employee_appraisal_tree', False)
-    #         return {
-    #             'domain': [('id', 'in', emp_appraisal)],
-    #             'name': 'Employee Appraisal',
-    #             'res_model': 'usl.employee.appraisal',
-    #             'type': 'ir.actions.act_window',
-    #             'views': [(tree_view_ref.id, 'tree'), (form_view_ref.id, 'form')],
-    #             'target': 'current',
-    #         }
+    def open_employee_appraisals(self):
+        pass 
+        # for rec in self:
+        #     appraisals = self.env['usl.employee.appraisal'].search([('employee_id', '=', self.id)])
+        #     emp_appraisal = [rec.id for rec in appraisals] if appraisals else []
+        #     form_view_ref = self.env.ref('maach_hr_appraisal.usl_employee_appraisal_form_view', False)
+        #     tree_view_ref = self.env.ref('maach_hr_appraisal.view_usl_employee_appraisal_tree', False)
+        #     return {
+        #         'domain': [('id', 'in', emp_appraisal)],
+        #         'name': 'Employee Appraisal',
+        #         'res_model': 'usl.employee.appraisal',
+        #         'type': 'ir.actions.act_window',
+        #         'views': [(tree_view_ref.id, 'tree'), (form_view_ref.id, 'form')],
+        #         'target': 'current',
+        #     }
 
-    # def stat_button_query(self):
-    #     pass
+    def stat_button_query(self):
+        pass
 
-    # def stat_button_number_commendation(self):
-    #     pass
+    def stat_button_number_commendation(self):
+        pass
 
-    # def stat_button_warning(self):
-    #     pass
+    def stat_button_warning(self):
+        pass
 
-    # def stat_button_absent(self):
-    #     pass
+    def stat_button_absent(self):
+        pass
 
-    # def stat_button_total_appraisal(self):
-    #     pass
+    def stat_button_total_appraisal(self):
+        pass
