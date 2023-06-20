fc32f72ba236c582c6ed21c0ab9c24f5c4b04ca4
Paul Ugwu <ugwu.paul.n@gmail.com>
2023-06-20 07:53:52 +0100

    [App] Billing Modulue

---
+++ billing/__manifest__.py
@@ -0,0 +1,22 @@
+##############################################################################
+#    Copyright (C) 2021. All Rights Reserved
+#    Enugu Electricity Distribution Company
+
+{
+    'name': 'Billing System',
+    'version': '14.0',
+    'author': "Maduka Chris Sopulu, Paul Ugwu",
+    'summary': 'Billing System: To enable the the generation  of bills for individuals',
+    'depends': ['base','hr'],
+    'description': "HR Performance Management",
+    "data": [
+            'security/security_view.xml',
+            #'security/ir_rule.xml',
+            #'security/ir.model.access.csv',
+            #'data/data.xml',
+            'views/res_partner_views.xml',
+
+    ],
+    'application': True,
+    "sequence": 6,
+}
---
+++ billing/models/res_partner.py
@@ -0,0 +1,4 @@
+from odoo import api, models, fields
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
